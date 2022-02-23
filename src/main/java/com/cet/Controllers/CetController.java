5a6fa92a398b1c538c35812598920b1a18740093
JNarvaezDiaz <susje999@hotmail.com>
2022-02-23 10:56:29 -0500

    Sent file uploading logic to service class

--- src/main/java/com/cet/Controllers/CetController.java
+++ src/main/java/com/cet/Controllers/CetController.java
@@ -1,14 +1,6 @@
 package com.cet.Controllers;
 
-import com.cet.Models.Cet;
-import com.cet.Models.InfoCet;
-import com.cet.Repositories.InfoCetRepository;
 import com.cet.Services.CetService;
-import com.cet.dtos.CetDto;
-import com.cet.utils.FileDelimitersUtil;
-import com.univocity.parsers.common.record.Record;
-import com.univocity.parsers.csv.CsvParser;
-import com.univocity.parsers.csv.CsvParserSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.InputStream;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController @RequestMapping("/cets")
 public class CetController {
@@ -29,66 +16,9 @@ public class CetController {
     @Autowired
     private CetService cetService;
 
-    @Autowired
-    private InfoCetRepository infoCetRepository;
-
     @PostMapping("/upload-data")
     public ResponseEntity<String> uploadData(@RequestParam("file") MultipartFile file) throws Exception {
-        if(cetService.findByNombreArchivo(file.getOriginalFilename())) {
-            return new ResponseEntity<>(
-                    "The file " + file.getOriginalFilename() + " has been already uploaded",
-                    HttpStatus.UNPROCESSABLE_ENTITY
-            );
-        }
-
-        Character delimiter = FileDelimitersUtil.returnDelimiter(new CsvParser(
-                new CsvParserSettings()).parseAllRecords(file.getInputStream()).toString(), '|', ',', ';');
-
-        List<InfoCet> infoCetList = new ArrayList<>();
-        InputStream inputStream = file.getInputStream();
-        CsvParserSettings settings = new CsvParserSettings();
-        settings.setHeaderExtractionEnabled(true);
-        settings.setDelimiterDetectionEnabled(true, delimiter);
-        CsvParser parser = new CsvParser(settings);
-
-        List<Record> parseAllRecords = parser.parseAllRecords(inputStream);
-
-        if (parseAllRecords.size() == 0) {
-            return new ResponseEntity<>("No content was found in the file", HttpStatus.NO_CONTENT);
-        }
-
-        CetDto cetDto = CetDto.builder().nombreArchivo(file.getOriginalFilename())
-                .fechaProceso(LocalDate.now())
-                .build();
-
-        Cet cet = cetService.save(cetDto);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        parseAllRecords.forEach(record -> {
-            InfoCet infoCet = InfoCet.builder()
-                    .numeroCaso(record.getString(0))
-                    .fechaDiagnostico(LocalDate.parse(record.getString(1), formatter))
-                    .bduaAfiliadoId(record.getString(2))
-                    .tipoId(record.getString(3))
-                    .identificacion(record.getString(4))
-                    .apellido1(record.getString(5))
-                    .apellido2(record.getString(6))
-                    .nombre1(record.getString(7))
-                    .nombre2(record.getString(8))
-                    .fechaNacimiento(LocalDate.parse(record.getString(9), formatter))
-                    .sexo(record.getString(10))
-                    .codEps(record.getString(11))
-                    .telefonoFijo(record.getString(12))
-                    .celular(record.getString(13))
-                    .covidContacto(Integer.parseInt(record.getString(14)))
-                    .cetId(cet.getId())
-                    .build();
-            infoCetList.add(infoCet);
-        });
-        infoCetRepository.saveAll(infoCetList);
-
-        return new ResponseEntity<>("Upload successfully", HttpStatus.CREATED);
+        return new ResponseEntity<>(cetService.uploadData(file), HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/cet/Services/CetService.java
+++ src/main/java/com/cet/Services/CetService.java
@@ -1,11 +1,23 @@
 package com.cet.Services;
 
 import com.cet.Models.Cet;
+import com.cet.Models.InfoCet;
 import com.cet.Repositories.CetRepository;
+import com.cet.Repositories.InfoCetRepository;
 import com.cet.dtos.CetDto;
+import com.cet.utils.FileDelimitersUtil;
+import com.univocity.parsers.common.record.Record;
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +26,9 @@ public class CetService {
 
     @Autowired
     private CetRepository cetRepository;
+
+    @Autowired
+    private InfoCetRepository infoCetRepository;
 
     public List<Cet> findAll() {
         return cetRepository.findAll();
@@ -32,6 +47,61 @@ public class CetService {
 
     public boolean findByNombreArchivo(String name) {
         return cetRepository.findByNombreArchivo(name);
+    }
+
+    public String uploadData(MultipartFile file) throws Exception {
+        if(findByNombreArchivo(file.getOriginalFilename())) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "The file " + file.getOriginalFilename() + " has been already uploaded");
+        }
+
+        Character delimiter = FileDelimitersUtil.returnDelimiter(new CsvParser(
+                new CsvParserSettings()).parseAllRecords(file.getInputStream()).toString(), '|', ',', ';');
+
+        List<InfoCet> infoCetList = new ArrayList<>();
+        InputStream inputStream = file.getInputStream();
+        CsvParserSettings settings = new CsvParserSettings();
+        settings.setHeaderExtractionEnabled(true);
+        settings.setDelimiterDetectionEnabled(true, delimiter);
+        CsvParser parser = new CsvParser(settings);
+
+        List<Record> parseAllRecords = parser.parseAllRecords(inputStream);
+
+        if (parseAllRecords.size() == 0) {
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No content was found in the file");
+        }
+
+        CetDto cetDto = CetDto.builder().nombreArchivo(file.getOriginalFilename())
+                .fechaProceso(LocalDate.now())
+                .build();
+
+        Cet cet = save(cetDto);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        parseAllRecords.forEach(record -> {
+            InfoCet infoCet = InfoCet.builder()
+                    .numeroCaso(record.getString(0))
+                    .fechaDiagnostico(LocalDate.parse(record.getString(1), formatter))
+                    .bduaAfiliadoId(record.getString(2))
+                    .tipoId(record.getString(3))
+                    .identificacion(record.getString(4))
+                    .apellido1(record.getString(5))
+                    .apellido2(record.getString(6))
+                    .nombre1(record.getString(7))
+                    .nombre2(record.getString(8))
+                    .fechaNacimiento(LocalDate.parse(record.getString(9), formatter))
+                    .sexo(record.getString(10))
+                    .codEps(record.getString(11))
+                    .telefonoFijo(record.getString(12))
+                    .celular(record.getString(13))
+                    .covidContacto(Integer.parseInt(record.getString(14)))
+                    .cetId(cet.getId())
+                    .build();
+            infoCetList.add(infoCet);
+        });
+        infoCetRepository.saveAll(infoCetList);
+
+        return "Upload successfully";
     }
 
 }
